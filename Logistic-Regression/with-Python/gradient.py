3ba95582772b7b71ace61f0602ff8e7c6690eb81
Abdelkarim EL-HAJJAMI <66040216+AbdelKarimEL-HAJJAMI@users.noreply.github.com>
2021-02-20 13:11:14 +0100

    Update gradient.py

--- Logistic-Regression/with-Python/gradient.py
+++ Logistic-Regression/with-Python/gradient.py
@@ -1,16 +1,16 @@
 import numpy as np
 from sigmoid import sigmoid
 
-def gradient(theta,X,y, lambda=None, flattenResult=False):
+def gradient(theta,X,y, reg_lambda=None, flattenResult=False):
      m,n = X.shape
      theta = theta.reshape((n,1))
      predictions = sigmoid(np.dot(X, theta))
      errors = np.subtract(predictions, y)
      grad = (1.0/m)*np.dot(X.T, errors)
          
-     if lambda:
+     if reg_lambda:
          grad0 = grad[0, :].reshape((1,1))
-         grad = grad[1:] + (lambda/m)*theta[1:]
+         grad = grad[1:] + (reg_lambda/m)*theta[1:]
          grad = np.r_[grad0, grad]
      
         
